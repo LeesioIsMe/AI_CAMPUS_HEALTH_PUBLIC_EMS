baade06bc471328af493806624e773942aaa568c
leesioisme <864823609@qq.com>
2020-11-30 19:05:17 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+@import url("./assets/css/common.css");
+</style>
